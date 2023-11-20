5192ceb74076612bf54822d051f5136e7a31b653
OmarENNEDDI <enneddiomar@gmail.com>
2023-11-20 19:10:42 +0100

    commiter des modiffication

--- BOOKIFY/SQL/bookify.sql
+++ BOOKIFY/SQL/bookify.sql
@@ -19,54 +19,62 @@ CREATE Table Equipements(
     Id SERIAL PRIMARY KEY,
     Id_Salles int,
     Nom VARCHAR (255),
-    Foreign Key (id_Salles) REFERENCES Salles(id)
+    Foreign Key (Id_Salles) REFERENCES Salles(Id)
 );
 
 CREATE Table Reservations(
      Id SERIAL PRIMARY KEY,
-     Id_Employees int ,
+     Id_Employees int,
      Id_Salles int,
      Date_Debut timestamp NOT NULL,
      Date_Fin timestamp NOT NULL,
-     Foreign Key (id_Employees) REFERENCES Employees(id),
-     Foreign Key (id_Salles) REFERENCES Salles(id)
+     Foreign Key (Id_Employees) REFERENCES Employees(Id),
+     Foreign Key (Id_Salles) REFERENCES Salles(Id)
 );
 
 ----------isertion des données dans la table employees-------------------------------
-INSERT INTO Employees (Nom, Email, Departement, Post)
+INSERT INTO Employees (Nom, Email, Departement, Post),
  VALUES 
- ('Omar', 'omar@gmail.com', 'Educatif', 'Formateur' )
- ('Mehdi', 'mehdi@gmail.com', 'Administration', 'Inspecteur' )
- ('Ghizlane', 'ghizlane@gmail.com', 'Administration', 'Conseillere' )
- ('Adnan', 'adnan@gmail.com', 'Educatif', 'Formateur' )
+ ('Omar', 'omar@gmail.com', 'Educatif', 'Formateur' ),
+ ('Mehdi', 'mehdi@gmail.com', 'Administration', 'Inspecteur' ),
+ ('Ghizlane', 'ghizlane@gmail.com', 'Administration', 'Conseillere' ),
+ ('Adnan', 'adnan@gmail.com', 'Educatif', 'Formateur' );
 
  ----------isertion des Données dans la table Salles-------------------------------
  INSERT INTO Salles(Nom, Capacité)
  VALUES 
- ('Breakin Code', 25)
- ('Reunion', 60)
- ('Agorra', 200)
- ('2b || 2b', 40)
+ ('Breakin Code', 25),
+ ('Reunion', 60),
+ ('Agorra', 200),
+ ('2b || 2b', 40);
 
   ----------isertion des Données dans la table d'Equipement-------------------------------
- INSERT INTO Salles(Id_Salles, Nom)
+ INSERT INTO Equipements(Id_Salles, Nom)
  VALUES 
- ( 3, 'Microphone' )
- ( 1, 'Tables' )
- ( 3, 'DataShow')
- ( 4, 'Chaises')
+ ( 3, 'Microphone' ),
+ ( 1, 'Tables' ),
+ ( 3, 'DataShow'),
+ ( 4, 'Chaises');
  
 
  ----------isertion des Dates dans la table reservations-------------------------------
  INSERT INTO Reservations (Id_Employees, Id_Salles, Date_Debut, Date_Fin)
  VALUES 
- (1, 1,'2023-10-11 10:00','2023-10-11 12:30' )
- (2, 2,'2023-10-13 09:00','2023-10-13 11:00' )
- (3, 3,'2023-10-14 14:00','2023-10-14 16:30' )
- (4, 4,'2023-10-15 08:00','2023-10-15 10:00' )
- (1, 3,'2023-10-22 10:00','2023-10-22 12:30' ) 
- (2, 4,'2023-11-04 08:00','2023-10-04 12:00' )
- (4, 1,'2023-10-06 14:00','2023-10-06 17:30' )
+ (1, 1,'2023-10-11 10:00','2023-10-11 12:30' ),
+ (2, 2,'2023-10-13 09:00','2023-10-13 11:00' ),
+ (3, 3,'2023-10-14 14:00','2023-10-14 16:30' ),
+ (4, 4,'2023-10-15 08:00','2023-10-15 10:00' ),
+ (1, 3,'2023-11-20 10:00','2023-11-20 12:30' ),
+ (2, 4,'2023-10-04 08:00','2023-10-04 12:00' ),
+ (4, 1,'2023-10-06 14:00','2023-10-06 17:30' ),
+ (3, 3,'2023-10-14 14:00','2023-10-14 16:30' ),
+ (4, 4,'2023-10-15 08:00','2023-10-15 10:00' ),
+ (1, 3,'2023-12-24 10:00','2023-12-24 12:30' ),
+ (2, 4,'2023-11-04 08:00','2023-11-04 12:00' ),
+ (4, 1,'2023-11-06 14:00','2023-11-06 17:30' ),
+ (3, 3,'2023-12-14 14:00','2023-12-14 16:30' ),
+ (4, 4,'2023-12-15 08:00','2023-12-15 10:00' ),
+ (3, 3,'2023-12-22 10:00','2023-12-22 12:30' );
 
   
   SELECT*FROM Employees
@@ -74,4 +82,8 @@ INSERT INTO Employees (Nom, Email, Departement, Post)
  
  SELECT*FROM Reservations
  WHERE Date_Debut > '02-11-2023';
+
+ 
+
+
 
